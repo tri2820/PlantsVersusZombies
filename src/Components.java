f8d339b564b67a39081b8457685aa373b06f179b
tidu0603 <pimon0603@gmail.com>
2019-10-10 23:58:42 +0700

    Added Speed and Damage components.

--- src/Components.java
+++ src/Components.java
@@ -63,4 +63,41 @@ class HealthComponent extends Component {
     }
 }
 
+class Speed extends Component {
+    // step define the delay time between each behavior of an entity
+    public int step = 10;
+    // amount of time added or subtracted on special events
+    public int step_change = 1;
+
+    public Speed() {
+        super();
+    }
+
+    void SpeedUp(String event) {
+        if (event == "fasten")
+            this.step -= step_change;//less delay -> faster
+    }
+
+    void SpeedDown(String event) {
+        if (event == "slowed")
+            this.step += step_change;//more delay -> longer
+    }
+}
+
+class Damage extends Component {
+    public Damage() {
+        super();
+    }
+    public int dmg = 1;
+    public int dmg_change = 1;
+    void SpeedUp(String event) {
+        if (event == "powered")
+            this.dmg -= dmg_change;//less delay -> faster
+    }
+
+    void SpeedDown(String event) {
+        if (event == "weaken")
+            this.dmg += dmg_change;//more delay -> longer
+    }
+}
 
