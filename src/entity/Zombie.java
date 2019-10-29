302d3c96f9719a36a2ff6bac85b7f7e36b00f6ff
Tri Nguyen <nguyenvantri2820@gmail.com>
2019-10-29 15:14:14 +0700

    Move entity methods of Components to GameSystemUtils

--- src/entity/Zombie.java
+++ src/entity/Zombie.java
@@ -5,19 +5,24 @@ import component.Health;
 import component.Position;
 import component.Size;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zombie extends Entity {
     public Zombie() {
+        this.add(new Collision(
+                new ArrayList<>(List.of(Zombie.class))
+        ));
+
         this.add(new Position(0, 0));
-        this.add(new Size());
-        this.add(new Collision());
+        this.add(new Size(10, 10));
         this.add(new Health(50, -7));
     }
 
-    public Zombie(int x, int y, int width, int height, int health, int on_collision_health_change) {
+    public Zombie(int x, int y) {
+        this();
+        this.remove(Position.class);
         this.add(new Position(x, y));
-        this.add(new Size(width, height));
-        this.add(new Collision());
-        this.add(new Health(health, on_collision_health_change));
     }
 }
 
